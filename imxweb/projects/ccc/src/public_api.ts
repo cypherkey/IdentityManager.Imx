d112dbe81ae629a1e6d06165f4e8847b4ba52e83
Aaron Anderson <aaron@netopia.ca>
2024-03-18 15:43:17 -0600

    Add commands to create component to file

--- imxweb/projects/ccc/src/public_api.ts
+++ imxweb/projects/ccc/src/public_api.ts
@@ -4,5 +4,11 @@
 
 export * from './lib/ccc.service';
 export * from './lib/ccc.module';
+
+/* 
+  New components created with following command:
+    ng generate component hello-dashboard-tile --project=ccc --style=scss --export --prefix=ccc --skip-tests
+    ng generate component goodbye-dashboard-tile --project=ccc --style=scss --export --prefix=ccc --skip-tests
+*/
 export * from './lib/hello-dashboard-tile/hello-dashboard-tile.component'
 export * from './lib/goodbye-dashboard-tile/goodbye-dashboard-tile.component'
